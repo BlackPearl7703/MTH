a62d742c1ed191c09bdc7fa012e6ed055ef722d8
Prince Koshti <princekoshti7703@gmail.com>
2025-08-10 15:35:37 +0530

    refactor : added observability

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, RouterProvider } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <BrowserRouter>
       <App />
+        <Analytics />
       {/* <RouterProvider  /> */}
     </BrowserRouter>
   // </StrictMode>,
