cf78f738436e823d4b5465ad4e4991bf4feab9a4
Prince Koshti <princekoshti7703@gmail.com>
2025-08-10 15:38:48 +0530

    refactor : fixed observability

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, RouterProvider } from 'react-router-dom'
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from '@vercel/analytics';
+
+
+inject();
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
       <App />
-        <Analytics />
+    
       {/* <RouterProvider  /> */}
     </BrowserRouter>
   // </StrictMode>,
